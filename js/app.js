b2af30210c6816f1047f5b42e7df71da389b291f
Abel <90abel.avr@gmail.com>
2023-06-04 04:02:45 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,34 @@
+const mascotaInput = document.querySelector('#mascota')
+const propietarioInput = document.querySelector('#propietario')
+const telefonoInput = document.querySelector('#telefono')
+const fechaInput = document.querySelector('#fecha')
+const horaInput = document.querySelector('#hora')
+const sintomasInput = document.querySelector('#sintomas')
+
+const formulario = document.querySelector("#nueva-cita")
+const contenedorCitas = document.querySelector("#citas")
+
+eventListeners()
+function eventListeners(){
+  mascotaInput.addEventListener('change',datosCita)
+  propietarioInput.addEventListener('change',datosCita)
+  telefonoInput.addEventListener('change',datosCita)
+  fechaInput.addEventListener('change',datosCita)
+  horaInput.addEventListener('change',datosCita)
+  sintomasInput.addEventListener('change',datosCita)
+ 
+}
+
+const citaObj = {
+  mascota: '',
+  propietario: '',
+  telefono: '',
+  fecha: '',
+  hora:'',
+  sintomas:''
+}
+
+function datosCita(e){
+  citaObj[e.target.name] = e.target.value
+  console.log(citaObj);
+}
